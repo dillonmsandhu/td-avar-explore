084e551cbd824031e21cdc07a89d4cd620a26c40
Dillon Sandhu <dillonmsandhu@gmail.com>
2025-12-11 16:27:38 -0500

    complete implementation of RND

--- deep/networks.py
+++ deep/networks.py
@@ -5,6 +5,77 @@ import jax.numpy as jnp
 import distrax
 from typing import Sequence
 
+class RND_Net(nn.Module):
+    activation: str = "tanh"
+
+    @nn.compact
+    def __call__(self, x):
+        if self.activation == "relu":
+            activation = nn.relu
+        else:
+            activation = nn.tanh
+        embedding = nn.Dense(
+            64, kernel_init=orthogonal(1.0), bias_init=constant(0.0)
+        )(x)
+        embedding = activation(embedding)
+        embedding = nn.Dense(
+            64, kernel_init=orthogonal(1.0), bias_init=constant(0.0)
+        )(embedding)
+        embedding = activation(embedding)
+
+        return embedding
+
+
+class Two_Head_ActorCritic(nn.Module):
+    action_dim: Sequence[int]
+    activation: str = "tanh"
+
+    @nn.compact
+    def __call__(self, x):
+        if self.activation == "relu":
+            activation = nn.relu
+        else:
+            activation = nn.tanh
+        actor_mean = nn.Dense(
+            64, kernel_init=orthogonal(jnp.sqrt(2)), bias_init=constant(0.0)
+        )(x)
+        actor_mean = activation(actor_mean)
+        actor_mean = nn.Dense(
+            64, kernel_init=orthogonal(jnp.sqrt(2)), bias_init=constant(0.0)
+        )(actor_mean)
+        actor_mean = activation(actor_mean)
+        actor_mean = nn.Dense(
+            self.action_dim, kernel_init=orthogonal(0.01), bias_init=constant(0.0)
+        )(actor_mean)
+        pi = distrax.Categorical(logits=actor_mean)
+
+        critic = nn.Dense(
+            64, kernel_init=orthogonal(jnp.sqrt(2)), bias_init=constant(0.0)
+        )(x)
+        critic = activation(critic)
+        critic = nn.Dense(
+            64, kernel_init=orthogonal(jnp.sqrt(2)), bias_init=constant(0.0)
+        )(critic)
+        critic = activation(critic)
+        critic = nn.Dense(1, kernel_init=orthogonal(0.001), bias_init=constant(0.0))(
+            critic
+        )
+
+        critic2 = nn.Dense(
+            64, kernel_init=orthogonal(jnp.sqrt(2)), bias_init=constant(0.0)
+        )(x)
+        critic2 = activation(critic2)
+        critic2 = nn.Dense(
+            64, kernel_init=orthogonal(jnp.sqrt(2)), bias_init=constant(0.0)
+        )(critic2)
+        critic2 = activation(critic2)
+        critic2 = nn.Dense(1, kernel_init=orthogonal(0.001), bias_init=constant(0.0))(
+            critic2
+        )
+
+        return pi, jnp.squeeze(critic, axis=-1), jnp.squeeze(critic2, axis=-1)
+
+
 class ActorCritic(nn.Module):
     action_dim: Sequence[int]
     activation: str = "tanh"
